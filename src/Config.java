2aa05ff4d85b4801349b836949d3ff71869a3801
neils <neil.sinclair@mac.com>
2025-06-21 17:02:54 +0100

    take2

--- src/Config.java
+++ src/Config.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -8,16 +9,22 @@ public class Config {
     private final Map<String, String> configMap = new HashMap<>();
 
     public Config(String filePath) throws IOException {
+
         Properties props = new Properties();
+
         try (FileInputStream in = new FileInputStream(filePath)) {
+
             props.load(in);
+
         }
         for (String name : props.stringPropertyNames()) {
+
             configMap.put(name, props.getProperty(name));
         }
     }
 
-    public String get(String key) {
+    public String getAsString(String key) {
+
         return configMap.get(key);
     }
 
@@ -59,6 +66,14 @@ public class Config {
         } catch (Exception e) {
             return defaultValue;
         }
+    }
+
+    //Add the string as bytes to a bytebuffer then flip for reading
+    public ByteBuffer putInByteBuffer(String key, ByteBuffer buffer) {
+
+        buffer.clear();
+
+        return buffer.put(configMap.get(key).getBytes()).flip();
     }
 
     public boolean containsKey(String key) {
--- src/FIXEngineLauncher.java
+++ src/FIXEngineLauncher.java
@@ -1,11 +1,22 @@
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.SelectionKey;
 
 public class FIXEngineLauncher {
 
     public static void main(String[] args) throws IOException {
+
+        Config conf  = new Config("c:\\Users\\neils\\IdeaProjects\\Sequencer2\\src\\sequencer.cfg");
+
+        ByteBuffer para = ByteBuffer.allocate(24);
+
+        conf.putInByteBuffer("Server.host",para);
+
+        String hostname = conf.getAsString("Server.hostname");
+
+        int port = conf.getAsInt("Server.port",0);
 
         EventLoop eventLoop = new EventLoop();
 
@@ -16,7 +27,7 @@ public class FIXEngineLauncher {
 
         dummyChannel.configureBlocking(false);
 
-        dummyChannel.connect(new java.net.InetSocketAddress("localhost", 9876));
+        dummyChannel.connect(new java.net.InetSocketAddress(hostname, port));
 
         eventLoop.registerIO(dummyChannel, SelectionKey.OP_CONNECT, channel -> {
 
